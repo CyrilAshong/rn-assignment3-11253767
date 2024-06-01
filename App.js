f839f22165b38ae0293ef29815228440006545d5
AshongCyril <cashong005@st.ug.edu.gh>
2024-06-01 00:53:45 +0000

    Imported the search.js from the component folder and implemented it

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Frame1 from './components/Frame1';
+import Search from './components/Search';
 
 export default function App() {
   return (
     <SafeAreaView>
       <Frame1 />
+      <Search />
     </SafeAreaView>
   );
 }
