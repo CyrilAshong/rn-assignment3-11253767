55348c775e52530d6617c0c529a1eef7e24c8094
AshongCyril <cashong005@st.ug.edu.gh>
2024-06-01 15:19:55 +0000

    Created two objects containing properties for the cards component and spread them in the Cards component

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
 import Frame1 from './components/Frame1';
 import Search from './components/Search';
+import Cards from './components/Cards';
 
 export default function App() {
+
+  const category1 = {
+    name: "Exercise",
+    taskno: "12 Task",
+    image: require('./Images/young woman working online.png')
+  };
+  const category2 = {
+    name: "Study",
+    taskno: "12 Task",
+    image: require('./Images/young woman working at desk.png')
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Frame1 />
       <Search />
+      <ScrollView style={styles.cardContainer}>
+        <Text style={styles.categoryText}>Categories</Text>
+        <ScrollView showsHorizontalScrollIndicator={false} horizontal={true}>
+          <Cards {...category1} />
+          <Cards {...category2} />
+          <Cards {...category2} />
+        </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 }
